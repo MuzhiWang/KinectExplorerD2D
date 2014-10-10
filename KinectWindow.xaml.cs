beb86970a02203d0681f460e92980dc7eec961b8
tangguang <tangguang1991@gmail.com>
2014-10-10 15:33:42 -0500

    add video

--- KinectWindow.xaml.cs
+++ KinectWindow.xaml.cs
@@ -13,6 +13,7 @@
     using Emgu.CV;
     using Emgu.CV.Structure;
     using System.Drawing;
+    using System.Threading;
     
     /// <summary>
     /// Interaction logic for KinectWindow.xaml
@@ -33,10 +34,14 @@
         /// Bitmap to display
         /// </summary>
         private WriteableBitmap colorBitmap = null;
+        private VideoWriter video;
+        private Thread isRecording;
+        private Mat img;
 
         public KinectWindow()
         {
 
+            
             this.kinectSensor = KinectSensor.GetDefault();
 
             // open the reader for the color frames
@@ -50,15 +55,20 @@
 
             // create the bitmap to display
             this.colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
-
+            
+            
             // open the sensor
             this.kinectSensor.Open();
 
             InitializeComponent();
+            System.Drawing.Size size = new System.Drawing.Size(colorFrameDescription.Width, colorFrameDescription.Height);
+            this.video = new VideoWriter(@"123.avi", -1, 25, size, true);
+
         }
 
         private void KinectWindow_Closing(object sender, CancelEventArgs e)
         {
+            video.Dispose();
         }
         /// <summary>
         /// Handles the color frame data arriving from the sensor
@@ -72,16 +82,19 @@
             {
                 if (colorFrame != null)
                 {
+                    //camera.Source = FrameToBitmap(colorFrame);
+                    
                     FrameDescription colorFrameDescription = colorFrame.FrameDescription;
                     int width = colorFrame.FrameDescription.Width;
                     int height = colorFrame.FrameDescription.Height;
-                    byte[] pixels = new byte[width * height * 4];
-                    colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
-                    //Image<Bgr, Byte> img = new Image<Bgr, byte>(BytesToBitmap(pixels));
-                    BitmapSource bitmapsource = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, pixels, width * 4);
-                    //Image<Bgra, Byte> img = new Image<Bgra, byte>(10, 10);
-                    camera.Source = bitmapsource;//BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, pixels, width * 4);
-                    /*using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
+                    //byte[] pixels = new byte[width * height * 4];
+                    //colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+                    //BitmapSource bitmapsource = BitmapSource.Create(width, height, 16, 16, PixelFormats.Bgra32, null, pixels, width * 4);
+                    //Image<Bgr, Byte> img = new Image<Bgr, byte>(BytesToBitmap(bitmapsource));
+                    // CvInvoke.cvShowImage("123", img);
+                    //camera.Source = bitmapsource;//BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, pixels, width * 4);
+                    // camera.Source = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, pixels, width * 4);
+                    using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
                     {
                         this.colorBitmap.Lock();
 
@@ -95,13 +108,34 @@
 
                             this.colorBitmap.AddDirtyRect(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight));
                         }
+                        
+                        Mat img1 = new Mat(height, width, 0, 4, this.colorBitmap.BackBuffer, width * 4);
+                        if (this.video != null)
+                        {
+                            this.video.Write(img1);
+                        }
+                        /*if (isRecording == null)
+                        {
+                            isRecording = new Thread(record);
+                            isRecording.Start();
+                        }
+                        else
+                        {
+                            isRecording = null;
 
+                        }*/
+
+
+                       
+                        //video.Dispose();
+                        //Image<Bgr, Byte> img1 = new Image<Bgr, byte>(img.Bitmap);
+                        //CvInvoke.cvShowImage("123", img1);
                         this.colorBitmap.Unlock();
                     }
                     if (this.colorBitmap != null)
                     {
                         camera.Source = this.colorBitmap;
-                    }*/
+                    }
                 }
             }
         }
@@ -119,5 +153,27 @@
             }
         }
 
+        public static BitmapSource FrameToBitmap(ColorFrame colorFrame)
+        {
+            FrameDescription colorFrameDescription = colorFrame.FrameDescription;
+            int width = colorFrame.FrameDescription.Width;
+            int height = colorFrame.FrameDescription.Height;
+            byte[] pixels = new byte[width * height * 4];
+            colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
+            BitmapSource bitmapsource = BitmapSource.Create(width, height, 16, 16, PixelFormats.Bgra32, null, pixels, width * 4);
+            Image<Bgr, Byte> img = new Image<Bgr, byte>(BytesToBitmap(bitmapsource));
+            return bitmapsource;
+        }
+
+
+       /* public void record()
+        {
+
+            while (this.isRecording != null)
+            {
+                this.video.Write(img);
+                Thread.Sleep(40);
+            }
+        }*/
     }
 }
