e5d1a4cbc46dbd55a03055f2e94d2b5acab35d60
tangguang <tangguang1991@gmail.com>
2014-10-10 18:31:10 -0500

    record video

--- KinectWindow.xaml.cs
+++ KinectWindow.xaml.cs
@@ -34,9 +34,9 @@
         /// Bitmap to display
         /// </summary>
         private WriteableBitmap colorBitmap = null;
-        private VideoWriter video;
-        private Thread isRecording;
-        private Mat img;
+        private VideoWriter video = null;
+        //private Thread isRecording;
+        //private Mat img;
 
         public KinectWindow()
         {
@@ -47,28 +47,32 @@
             // open the reader for the color frames
             this.colorFrameReader = this.kinectSensor.ColorFrameSource.OpenReader();
 
-            // wire handler for frame arrival
-            this.colorFrameReader.FrameArrived += this.Reader_ColorFrameArrived;
+           
 
             // create the colorFrameDescription from the ColorFrameSource using Bgra format
             FrameDescription colorFrameDescription = this.kinectSensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
 
             // create the bitmap to display
             this.colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
-            
-            
+
+            System.Drawing.Size size = new System.Drawing.Size(colorFrameDescription.Width, colorFrameDescription.Height);
+            this.video = new VideoWriter(@"123.mp4", -1, 10, size, true);
+
+            // wire handler for frame arrival
+            this.colorFrameReader.FrameArrived += this.Reader_ColorFrameArrived;
+             
             // open the sensor
             this.kinectSensor.Open();
-
             InitializeComponent();
-            System.Drawing.Size size = new System.Drawing.Size(colorFrameDescription.Width, colorFrameDescription.Height);
-            this.video = new VideoWriter(@"123.avi", -1, 25, size, true);
-
         }
 
         private void KinectWindow_Closing(object sender, CancelEventArgs e)
         {
-            video.Dispose();
+            if (this.video != null)
+            {
+                this.video.Dispose();
+                this.video = null;
+            }
         }
         /// <summary>
         /// Handles the color frame data arriving from the sensor
@@ -87,33 +91,26 @@
                     FrameDescription colorFrameDescription = colorFrame.FrameDescription;
                     int width = colorFrame.FrameDescription.Width;
                     int height = colorFrame.FrameDescription.Height;
-                    //byte[] pixels = new byte[width * height * 4];
-                    //colorFrame.CopyConvertedFrameDataToArray(pixels, ColorImageFormat.Bgra);
-                    //BitmapSource bitmapsource = BitmapSource.Create(width, height, 16, 16, PixelFormats.Bgra32, null, pixels, width * 4);
-                    //Image<Bgr, Byte> img = new Image<Bgr, byte>(BytesToBitmap(bitmapsource));
-                    // CvInvoke.cvShowImage("123", img);
-                    //camera.Source = bitmapsource;//BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, pixels, width * 4);
-                    // camera.Source = BitmapSource.Create(width, height, 96, 96, PixelFormats.Bgra32, null, pixels, width * 4);
                     using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
                     {
                         this.colorBitmap.Lock();
 
                         // verify data and write the new color frame data to the display bitmap
-                        if ((colorFrameDescription.Width == this.colorBitmap.PixelWidth) && (colorFrameDescription.Height == this.colorBitmap.PixelHeight))
+                        if ((width == this.colorBitmap.PixelWidth) && (height == this.colorBitmap.PixelHeight))
                         {
                             colorFrame.CopyConvertedFrameDataToIntPtr(
                                 this.colorBitmap.BackBuffer,
-                                (uint)(colorFrameDescription.Width * colorFrameDescription.Height * 4),
+                                (uint)(width * height * 4),
                                 ColorImageFormat.Bgra);
-
                             this.colorBitmap.AddDirtyRect(new Int32Rect(0, 0, this.colorBitmap.PixelWidth, this.colorBitmap.PixelHeight));
-                        }
+                        }        
                         
-                        Mat img1 = new Mat(height, width, 0, 4, this.colorBitmap.BackBuffer, width * 4);
                         if (this.video != null)
                         {
+                            Mat img1 = new Mat(height, width, 0, 4, this.colorBitmap.BackBuffer, width * 4);
                             this.video.Write(img1);
                         }
+
                         /*if (isRecording == null)
                         {
                             isRecording = new Thread(record);
@@ -124,14 +121,9 @@
                             isRecording = null;
 
                         }*/
-
-
-                       
-                        //video.Dispose();
-                        //Image<Bgr, Byte> img1 = new Image<Bgr, byte>(img.Bitmap);
-                        //CvInvoke.cvShowImage("123", img1);
                         this.colorBitmap.Unlock();
                     }
+
                     if (this.colorBitmap != null)
                     {
                         camera.Source = this.colorBitmap;
